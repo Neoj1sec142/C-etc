e9e579573052ccc91134939b566e94c20675d5d3
Mark Harmon <neothej1sec@gmail.com>
2022-11-17 14:49:46 -0600

    worked with string concatenation and variables

---
+++ C#/CSharpGroundUp/Program1/Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program1
+{
+    static void Main(){
+        Console.WriteLine("Please enter your first name: ");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name: ");
+        string LastName = Console.ReadLine();
+        
+        // Concatenate sting Method
+        // Console.WriteLine("Hello " + FirstName + " " + LastName);
+        
+        // Placeholder Method
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+        
+        // String Literal Method
+        // Console.WriteLine($"Hello {FirstName} {LastName}");
+    }
+}
+
---
+++ C#/CSharpGroundUp/notes1.md
@@ -0,0 +1,4 @@
+# Intro to C Sharp and .Net Framework:
+
+**Using the namespace decalration, using System; indicates that you arte using the system namespace**
+- A namespace is used to organize your code and is a collection of classes interfaces, structs, enums, and delegates.
